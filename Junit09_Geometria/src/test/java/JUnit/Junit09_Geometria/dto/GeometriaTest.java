601ac870b8d0999f82f614288eb8b9ac86d4e9a7
Jimxx22 <jaumegil22@gmail.com>
2022-08-05 10:10:58 +0200

    Creacion

---
+++ Junit09_Geometria/src/test/java/JUnit/Junit09_Geometria/dto/GeometriaTest.java
@@ -0,0 +1,13 @@
+package JUnit.Junit09_Geometria.dto;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GeometriaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
