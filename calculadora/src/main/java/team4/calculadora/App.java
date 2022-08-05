37599cea4e1070b84677a8721ff9a96df79f218a
Jimxx22 <jaumegil22@gmail.com>
2022-08-05 11:23:54 +0200

    Calculadora

---
+++ calculadora/src/main/java/team4/calculadora/App.java
@@ -0,0 +1,13 @@
+package team4.calculadora;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
