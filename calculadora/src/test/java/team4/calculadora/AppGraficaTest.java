51a6c4b569ffadaff9a7a33ca083cbf433799cff
Marccudi <marccudi@hotmail.es>
2022-08-08 19:26:18 +0200

    Merge branch 'main' of https://github.com/Marccudi/C4-UD21-Java-Team4.git into main

---
+++ calculadora/src/test/java/team4/calculadora/AppGraficaTest.java
@@ -0,0 +1,12 @@
+package team4.calculadora;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+public class AppGraficaTest {
+
+	@Test
+	public void test() {
+		System.out.println("Hola");
+	}
+}
--- calculadora/src/test/java/team4/calculadora/AppTest.java
+++ calculadora/src/test/java/team4/calculadora/AppTest.java
@@ -2,13 +2,20 @@ package team4.calculadora;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@BeforeEach
+	public void before() {
+		System.out.println("before()");
+	}
+	
+	@AfterEach
+	public void after() {
+		System.out.println("after()");
 	}
 
 }
